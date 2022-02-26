406cdce9703f7ca4baf68121edd5c441bf57eef0
Adam <adam.crespo101@outlook.com>
2022-02-26 11:12:10 -0500

    commit tree 8892e3384c6906ed0dfbee63f96e5e2880691896

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solutions in this file
+ const names = ['Guadalupe', 'Ollie', 'Aki'];
+
+// function writeCards(names){
+//     let cards = []
+//     for (let i = 0; i < names.length; i++){
+//         cards.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`) 
+//         debugger;
+        
+//     } 
+//     return cards;
+// }
+    
+// function countDown(i){
+//     while (i > 0){
+//        console.log(i)
+//        i -=1
+//     }
+//     console.log(i)
+// }
+
+const writeCards = (names) =>{
+    let cards = []
+    for(let i = 0; i < names.length; i++)
+    cards.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    return cards;
+}
+
+const countDown = (index) => {
+    while (index > 0){
+        console.log(index)
+        index -= 1;
+    }
+        console.log(index)
+}
